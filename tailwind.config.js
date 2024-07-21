54fac98f9511694aa0801f69067873ca2827d5dc
rainbow4prod <buyvol72@gmail.com>
2024-07-21 21:04:13 +0300

    React initial structure

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // Enable dark mode class
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
