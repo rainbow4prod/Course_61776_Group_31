4491afff28c82261bd92fd461e75c84e245ba535
rainbow4prod <buyvol72@gmail.com>
2024-08-02 15:39:16 +0300

    .

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/braudana">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-contact" element={<AboutAndContact />} />
